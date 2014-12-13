5750d0997ad532c5b545cf5280428a6754691bd9
EricPHamilton <ehamilton13@gmail.com>
2014-12-13 01:35:44 -0500

    CMD line will now output given command

--- src/CommandWindow.java
+++ src/CommandWindow.java
@@ -10,12 +10,13 @@ import javax.swing.JTextArea;
 import javax.swing.JTextPane;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 
 public class CommandWindow {
 
 	private JFrame frame;
-	private JTextField txtTestingText;
 
 	/**
 	 * Launch the application.
@@ -43,25 +44,51 @@ public class CommandWindow {
 	/**
 	 * Initialize the contents of the frame.
 	 */
+	static JTextPane textPane;
+	JTextField txtBox;
 	private void initialize() {
+		
 		frame = new JFrame();
+		textPane = new JTextPane();
+
 		frame.setBounds(100, 100, 508, 339);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		txtTestingText = new JTextField();
-		txtTestingText.setFont(new Font("Consolas", Font.PLAIN, 12));
-		txtTestingText.setToolTipText("Enter commands here");
-		txtTestingText.setBackground(new Color(255, 255, 255));
-		txtTestingText.setForeground(new Color(0, 0, 0));
-		frame.getContentPane().add(txtTestingText, BorderLayout.SOUTH);
-		txtTestingText.setColumns(10);
-		
-		JTextPane textPane = new JTextPane();
+		txtBox = new JTextField();
+		txtBox.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					String cmd = txtBox.getText();
+					txtBox.setText("");
+					addToTextPane(cmd);
+				}
+			}
+		});
+		txtBox.setFont(new Font("Consolas", Font.PLAIN, 12));
+		txtBox.setToolTipText("Enter commands here");
+		txtBox.setBackground(new Color(255, 255, 255));
+		txtBox.setForeground(new Color(0, 0, 0));
+		frame.getContentPane().add(txtBox, BorderLayout.SOUTH);
+		txtBox.setColumns(10);
+	
 		textPane.setFont(new Font("Consolas", Font.PLAIN, 12));
 		textPane.setEditable(false);
 		textPane.setToolTipText("Command output");
 		textPane.setBackground(new Color(204, 204, 204));
 		frame.getContentPane().add(textPane, BorderLayout.CENTER);
+		
 	}
 
+	private void addToTextPane(String cmd) {
+		String previousText = textPane.getText();
+		String futureText = previousText;
+		if (!previousText.equals("")) {
+			futureText += "\n";
+		}
+		
+		//TODO: Change to command output.
+		futureText += cmd;
+		textPane.setText(futureText);
+	}
 }
