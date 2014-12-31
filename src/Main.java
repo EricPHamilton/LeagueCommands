f2450e4474bd64140d12e8e8d2abf586d77e3bd8
EricPHamilton <ehamilton13@gmail.com>
2014-12-31 18:05:41 -0500

    Added Documentation for Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
-
+/**
+ * This classes purpose is to provide a starting point for anyone
+ * interested in studying this program. Instead of having to look
+ * through all classes for the main method, I thought it would be
+ * easier to define the main method in a class called "Main".
+ */
 public class Main {
 	public static void main(String[] args) {
 		CommandWindow.runGUI();
